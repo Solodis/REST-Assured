b705d8289ccc63a167058b98a606abb3b67685ed
Vyacheslav_Utenkov <Vyacheslav_Utenkov@epam.com>
2018-04-02 15:47:16 +0300

    Commit for a conflict for the Master

---
+++ src/main/java/epam/rest/utenkov/bo/TestingClass.java
@@ -0,0 +1,11 @@
+package epam.rest.utenkov.bo;
+
+public class TestingClass {
+	private Widgets widget;
+
+	public TestingClass(Widgets widget) {
+		super();
+		this.widget = widget;
+		System.out.println("Master");
+	}
+}
